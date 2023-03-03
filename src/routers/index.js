890993c38c15ef5b6eabe5069fcb1940dbbd6d42
FelipeReis1 <feliperg15@yahoo.com.br>
2023-03-03 14:46:21 -0300

    feat: added ranking router

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import authRouter from "./authRouter.js";
 import urlRouter from "./urlRouter.js";
 import userRouter from "./userRouter.js";
+import rankingRouter from "./rankingRouter.js";
 
 const route = Router();
 
 route.use(authRouter);
 route.use(urlRouter);
 route.use(userRouter);
+route.use(rankingRouter);
 
 export default route;
