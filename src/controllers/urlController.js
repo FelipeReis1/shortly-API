c6955a52a94791d1ee968292f5f4cc9b248e0827
FelipeReis1 <feliperg15@yahoo.com.br>
2023-03-03 00:42:44 -0300

    fix: try catch blocks and check if id exists

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -5,27 +5,34 @@ export async function shortenUrl(req, res) {
   const { url } = req.body;
   const token = res.locals;
   const shortenedUrl = nanoid();
-
-  const session = await db.query(`SELECT * FROM sessions WHERE token = $1`, [
-    token,
-  ]);
-  await db.query(
-    `INSERT INTO urls (url,"userId", "shortUrl") VALUES ($1, $2, $3)`,
-    [url, session.rows[0].userId, shortenedUrl]
-  );
-  res.status(201).send({ id: session.rows[0].id, shortUrl: shortenedUrl });
+  try {
+    const session = await db.query(`SELECT * FROM sessions WHERE token = $1`, [
+      token,
+    ]);
+    await db.query(
+      `INSERT INTO urls (url,"userId", "shortUrl") VALUES ($1, $2, $3)`,
+      [url, session.rows[0].userId, shortenedUrl]
+    );
+    res.status(201).send({ id: session.rows[0].id, shortUrl: shortenedUrl });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 }
 
 export async function getUrlById(req, res) {
-  const url = (await db.query(`SELECT * FROM urls`)).rows[0].url;
-  const shortUrl = (await db.query(`SELECT * FROM urls`)).rows[0].shortUrl;
-  const { id } = req.params;
-  if (!url) {
-    return res.sendStatus(404);
+  try {
+    const url = (await db.query(`SELECT * FROM urls`)).rows[0].url;
+    const shortUrl = (await db.query(`SELECT * FROM urls`)).rows[0].shortUrl;
+    const { id } = req.params;
+    if (!id) {
+      return res.sendStatus(404);
+    }
+    res.status(200).send({
+      id: id,
+      shortUrl,
+      url,
+    });
+  } catch (error) {
+    res.status(500).send(error.message);
   }
-  res.status(200).send({
-    id: id,
-    shortUrl,
-    url,
-  });
 }
