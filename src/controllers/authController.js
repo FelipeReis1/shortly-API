7fe7201e8829426bba1d6576ecab9e9bd9704f2f
FelipeReis1 <feliperg15@yahoo.com.br>
2023-03-02 23:07:19 -0300

    feat: sign in controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,6 @@
 import { db } from "../database/database.connection.js";
 import bcrypt from "bcrypt";
+import { v4 as uuidV4 } from "uuid";
 export async function createUser(req, res) {
   const { name, email, password } = req.body;
   const encryptedPassword = bcrypt.hashSync(password, 10);
@@ -16,6 +17,31 @@ export async function createUser(req, res) {
       [name, email, encryptedPassword]
     );
     res.sendStatus(201);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
+
+export async function loginUser(req, res) {
+  const { email, password } = req.body;
+
+  try {
+    const user = await db.query(`SELECT * FROM users WHERE email = $1`, [
+      email,
+    ]);
+
+    if (
+      user.rowCount === 0 ||
+      !bcrypt.compareSync(password, user.rows[0].password)
+    ) {
+      return res.sendStatus(401);
+    }
+    const token = uuidV4();
+    await db.query(`INSERT INTO sessions (token,"userId") VALUES ($1, $2)`, [
+      token,
+      user.rows[0].id,
+    ]);
+    res.status(200).send(token);
   } catch (error) {
     res.status(500).send(error.message);
   }
