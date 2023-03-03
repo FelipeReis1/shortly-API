e258c7a0a42c1e40fd2a4b9492069bb5bad3fc2b
FelipeReis1 <feliperg15@yahoo.com.br>
2023-03-02 23:07:52 -0300

    feat: sign in route

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { createUser } from "../controllers/authController.js";
+import { createUser, loginUser } from "../controllers/authController.js";
 import { validateSchema } from "../middlewares/schemasValidator.js";
 import signUpSchema from "../schemas/signUpSchema.js";
+import signInSchema from "../schemas/signInSchema.js";
 
 const route = Router();
 
 route.post("/signup", validateSchema(signUpSchema), createUser);
+route.post("/signin", validateSchema(signInSchema), loginUser);
 
 export default route;
