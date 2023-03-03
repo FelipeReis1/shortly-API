79e61aaa2f3b71c06607699f81c7cb2ca98835e9
FelipeReis1 <feliperg15@yahoo.com.br>
2023-03-03 14:23:09 -0300

    feat: added user router

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import authRouter from "./authRouter.js";
 import urlRouter from "./urlRouter.js";
+import userRouter from "./userRouter.js";
+
 const route = Router();
 
 route.use(authRouter);
 route.use(urlRouter);
+route.use(userRouter);
 
 export default route;
